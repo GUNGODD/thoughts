d9b0bed38d6fe8b51f7dbbfac973edbafd68fad0
anuragnegi000 <anurag.negi862@gmail.com>
2024-05-20 07:38:34 +0530

    added all basic route required for a blog app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,39 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+const app = new Hono();
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.post("/api/v1/signup",(c)=>{
+  return c.json({
+    message:"This is working"
+  })
+})
+
+app.post("api/v1/login",(c)=>{
+  return c.json({
+    message:"Login route is also working"
+  })
+})
+
+app.post("/api/v1/blog",(c)=>{
+  return c.json({
+    message:"Blog post route also working"
+  })
+})
+
+app.put("/api/v1/blog",(c)=>{
+  return c.json({
+    message:"Blog PUT api is also working"
+  })
+})
+
+app.get("/api/v1/blog",(c)=>{
+  return c.json({
+    message:"Getting all blogs"
+  })
 })
 
 export default app
