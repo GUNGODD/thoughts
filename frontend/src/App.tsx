3a72a9ef26f2ed9c38eac4f34e6154e05cb7367e
anuragnegi000 <anurag.negi862@gmail.com>
2024-07-07 17:30:45 +0530

    completion of quote area

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Signup } from './pages/Signup'
+import { Signin } from './pages/Signin'
+import { Blog } from './pages/Blog'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      hi
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/blog/:id" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,10 @@
+import { Quote } from "../components/Quote"
+
+export const Signup=()=>{
+    return <div className="grid grid-cols-2">
+        <div className="flex justify-center text-2xl font-bold">
+            Signup
+        </div>
+        <Quote/>
+    </div>
+}
